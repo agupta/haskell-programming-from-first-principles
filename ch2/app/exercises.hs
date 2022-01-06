bd9b9429b411e3e2c8b8db7e9c6260bb8410eca8
Anish Gupta <anish-gupta@outlook.com>
2022-01-06 18:43:19 +0000

    add code

---
+++ ch2/app/exercises.hs
@@ -0,0 +1,18 @@
+-- Section 2.5
+-- 1. Prepend let onto them, not required in newer versions however.
+
+
+-- 2.
+
+threepointonefoursquare :: Double -> Double
+threepointonefoursquare x = 3.14 * (x*x)
+
+-- 3.
+
+pisquare :: Double -> Double
+pisquare x = pi * (x*x)
+
+-- Section 2.6
+-- 1. a =/ b
+-- 2. a = b
+-- 3. a /=b
